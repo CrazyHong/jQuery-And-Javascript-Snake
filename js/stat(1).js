ddc3e2dcda3cdfb8178caf97a6894d154e4a17b1
CrazyHong <blackj2015@hotmail.com>
2017-10-25 15:07:47 +0800

    commit

---
+++ js/stat(1).js
@@ -0,0 +1,15 @@
+/* Baidu Stat */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?ca530285b593ad370c84521aaa2b9520";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+/* ImagePlus
+document.writeln("<script>");
+document.writeln("var cpro_id = \'u1669450\';");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/i.js\"><\/script>") */
+/* 51.la 
+document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/3650558.js"></script>'); */
